84c0ffcaa74706fc8f3e6bc0f1f42abc27c47257
Anrews Costa Chiozo <andrewschiozo@gmail.com>
2025-10-22 10:58:34 -0300

    refactor: aplicação do DTO de entrada

--- src/Application/UseCases/RegistrarBoletoUseCase.php
+++ src/Application/UseCases/RegistrarBoletoUseCase.php
@@ -3,13 +3,14 @@ declare(strict_types= 1);
 
 namespace AndrewsChiozo\ApiCobrancaBb\Application\UseCases;
 
+use AndrewsChiozo\ApiCobrancaBb\Application\DTO\RegistrarBoletoRapidoDTO;
 use AndrewsChiozo\ApiCobrancaBb\Exceptions\HttpCommunicationException;
 use AndrewsChiozo\ApiCobrancaBb\Ports\FormatterInterface;
 use AndrewsChiozo\ApiCobrancaBb\Ports\HttpClientInterface;
 use AndrewsChiozo\ApiCobrancaBb\Ports\ResponseParserInterface;
-use AndrewsChiozo\ApiCobrancaBb\Ports\UseCaseInterface;
+use InvalidArgumentException;
 
-class RegistrarBoletoUseCase implements UseCaseInterface
+class RegistrarBoletoUseCase
 {
  
     public function __construct(
@@ -19,14 +20,15 @@ class RegistrarBoletoUseCase implements UseCaseInterface
     )
     { }
 
-        /**
+    /**
      * Envia os dados para a API do BB e registra uma nova cobrança.
      * 
-     * @param array $cobrancaData Dados da cobrança
+     * @param RegistrarBoletoRapidoDTO $cobrancaData Dados da cobrança
      * @return array Retorna os dados da Cobrança criada
+     * 
      * @throws HttpCommunicationException Se houver falha na comunicação.
      */
-    public function execute(?array $cobrancaData = null): array
+    public function execute(RegistrarBoletoRapidoDTO $cobrancaData): array
     {
         $payload = $this->formatter->format($cobrancaData);
         $uri = '/cobrancas/v2/boletos';
