28f6d024ec4f6d68b59fae085aa622513034d8fc
Anrews Costa Chiozo <andrewschiozo@gmail.com>
2026-04-27 16:03:21 -0300

    refactor: alterar tipos dos atributos do dto para tipos escalares

--- src/Application/DTO/DetalharBoletoDTO.php
+++ src/Application/DTO/DetalharBoletoDTO.php
@@ -1,46 +1,23 @@
 <?php
+
 declare(strict_types= 1);
 
 namespace AndrewsChiozo\ApiCobrancaBb\Application\DTO;
 
-use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\NossoNumeroVO;
-use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\NumeroConvenioVO;
-use AndrewsChiozo\ApiCobrancaBb\Ports\DTOInterface;
 use InvalidArgumentException;
 
-class DetalharBoletoDTO implements DTOInterface
+class DetalharBoletoDTO
 {
     public function __construct(
-        public readonly NumeroConvenioVO $numeroConvenio,
-        public readonly NossoNumeroVO $nossoNumero
+        public string $numeroConvenio,
+        public string $nossoNumero
     ) {}
 
-    public static function fromArray(array $data): DetalharBoletoDTO
+    public static function fromArray(array $data): self
     {
-        try{            
-            self::validarInput($data);
-
-            $numeroConvenio = new NumeroConvenioVO($data['numeroConvenio']);
-            $nossoNumero = new NossoNumeroVO($data['nossoNumero']);
-
-            return new self(
-                numeroConvenio: $numeroConvenio,
-                nossoNumero: $nossoNumero,
-            );
-        } catch(\Exception $e){
-            throw $e;
-        }
+        return new self(
+            numeroConvenio: $data['numeroConvenio'] ?? throw new InvalidArgumentException('Convênio não informado'),
+            nossoNumero: $data['nossoNumero'] ?? throw new InvalidArgumentException('Nosso número não informado'),
+        );
     }
-
-    private static function validarInput(array $data): void
-    {
-        $indicesObrigatorios = ['numeroConvenio', 'nossoNumero'];
-    
-        foreach($indicesObrigatorios as $indice){
-            if(!isset($data[$indice])){
-                throw new InvalidArgumentException("O campo {$indice} é obrigatório.");
-            }
-        }
-    }
-
 }
