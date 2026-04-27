dc7f19e3295eb926664eee0eefe92eb5b0f5220a
Anrews Costa Chiozo <andrewschiozo@gmail.com>
2026-04-27 19:09:21 -0300

    refactor: ajustar a formatação a partir do DTO com tipos escalares

--- src/Domain/Services/RegistrarBoletoFormatter.php
+++ src/Domain/Services/RegistrarBoletoFormatter.php
@@ -3,39 +3,59 @@ declare(strict_types=1);
 
 namespace AndrewsChiozo\ApiCobrancaBb\Domain\Services;
 
-use AndrewsChiozo\ApiCobrancaBb\Application\DTO\RegistrarBoletoRapidoDTO;
+use AndrewsChiozo\ApiCobrancaBb\Application\DTO\RegistrarBoletoDTO;
+use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\DocumentoVO;
 use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\IdentificadorBoleto;
-use AndrewsChiozo\ApiCobrancaBb\Ports\DTOInterface;
-use AndrewsChiozo\ApiCobrancaBb\Ports\FormatterInterface;
+use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\NossoNumeroVO;
+use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\NumeroConvenioVO;
+use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\PagadorVO;
+use AndrewsChiozo\ApiCobrancaBb\Domain\ValueObjects\ValorTituloVO;
+use DateTimeImmutable;
 
 /**
  * Serviço responsável por formatar e validar os dados internos de uma Cobrança
  * no payload JSON exigido pela API do Banco do Brasil.
  */
-class RegistrarBoletoFormatter implements FormatterInterface
+class RegistrarBoletoFormatter
 {
     /**
      * Transforma os dados da Cobrança em um array compatível com o payload da API.
      * * @param array $cobrancaData Dados internos (ex: ['valor' => 100.50, 'cliente' => '...'])
      * @return array Payload pronto para ser enviado via HTTP
      */
-    public function format(DTOInterface $dto): array
+    public function format(RegistrarBoletoDTO $dto): array
     {
-        if( !$dto instanceof RegistrarBoletoRapidoDTO ) {
-            throw new \InvalidArgumentException('Tipo de dado inválido. Esperado: ' . RegistrarBoletoRapidoDTO::class);
+        if( !$dto instanceof RegistrarBoletoDTO ) {
+            throw new \InvalidArgumentException('Tipo de dado inválido. Esperado: ' . RegistrarBoletoDTO::class);
         }
 
-        $numeroTituloCliente = IdentificadorBoleto::create($dto->numeroConvenio, $dto->nossoNumero)->identificadorCompleto;
+        $convenio = new NumeroConvenioVO($dto->numeroConvenio);
+        $nossoNumero = new NossoNumeroVO($dto->nossoNumero);
+        $dataVencimento = new DateTimeImmutable($dto->dataVencimento);
+        $valortitulo = new ValorTituloVO($dto->valorTitulo);
+        $pagador = new PagadorVO(
+            documento: new DocumentoVO($dto->pagadorNumeroDocumento),
+            nome: $dto->pagadorNome ?? null,
+            endereco: $dto->pagadorEndereco ?? null,
+            cep: $dto->pagadorCep,
+            cidade: $dto->pagadorCidade ?? null,
+            bairro: $dto->pagadorBairro ?? null,
+            uf: $dto->pagadorUf ?? 'SP',
+            telefone: $dto->pagadorTelefone ?? null,
+            email: $dto->pagadorEmail ?? null,
+        );
+
+        $numeroTituloCliente = IdentificadorBoleto::create($convenio, $nossoNumero)->identificadorCompleto;
 
         return [
-            'numeroConvenio' => $dto->numeroConvenio->numero, 
-            'dataVencimento' => $dto->dataVencimento->format('d.m.Y'),
-            'valorOriginal' => $dto->valorTitulo->valor,
+            'numeroConvenio' => $convenio->numero, 
+            'dataVencimento' => $dataVencimento->format('d.m.Y'),
+            'valorOriginal' => $valortitulo->formatadoParaBB(),
             'numeroTituloCliente' => $numeroTituloCliente,
             'pagador' => [
-                'tipoInscricao' => $dto->pagador->documento->tipo->value,
-                'numeroInscricao' => $dto->pagador->documento->valor,
-                'cep' => $dto->pagador->cep
+                'tipoInscricao' => $pagador->documento->tipo->value,
+                'numeroInscricao' => $pagador->documento->valor,
+                'cep' => $pagador->cep
             ]
         ];
     }
