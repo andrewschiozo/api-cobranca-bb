86787b64ff27c0af42b7774fc7675f1114ac5718
Anrews Costa Chiozo <andrewschiozo@gmail.com>
2026-05-09 00:11:04 -0300

    refactor: remover atribuição de null

--- src/Application/DTO/AutenticarDTO.php
+++ src/Application/DTO/AutenticarDTO.php
@@ -3,34 +3,22 @@ declare(strict_types= 1);
 
 namespace AndrewsChiozo\ApiCobrancaBb\Application\DTO;
 
-class AutenticarDTO
+readonly class AutenticarDTO
 {
     public function __construct(
-        public readonly ?string $authUrl,
-        public readonly ?string $clientId,
-        public readonly ?string $clientSecret,
-        public readonly ?string $scope,
+        public string $authUrl,
+        public string $clientId,
+        public string $clientSecret,
+        public string $scope,
     ) {}
 
-    /**
-     * Cria uma instância de AutenticarDTO a partir de um array de dados.
-     * @param array $data
-    * [
-    *      'authUrl': 'https://api.bb.com.br/oauth/token',
-    *      'clientId': 'client-id',
-    *      'clientSecret': 'client-secret',
-    *      'appKey': 'app-key',
-    *      'accessToken': 'access-token'
-    * ]
-     * @return AutenticarDTO
-     */
     public static function fromArray(array $data): AutenticarDTO
     {
         return new self(
-            authUrl: $data['authUrl'] ?? null,
-            clientId: $data['clientId'] ?? null,
-            clientSecret: $data['clientSecret'] ?? null,
-            scope: $data['scope'] ?? null,
+            authUrl: $data['authUrl'],
+            clientId: $data['clientId'],
+            clientSecret: $data['clientSecret'],
+            scope: $data['scope'],
         );
     }
 }
