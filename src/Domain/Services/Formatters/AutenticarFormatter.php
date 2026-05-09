14d692e4c53796bd9aaf96d60c8deb649c82e458
Anrews Costa Chiozo <andrewschiozo@gmail.com>
2026-05-09 00:38:12 -0300

    feat: formatar payload e header p/ autenticacao

--- src/Domain/Services/Formatters/AutenticarFormatter.php
+++ src/Domain/Services/Formatters/AutenticarFormatter.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace AndrewsChiozo\ApiCobrancaBb\Domain\Services\Formatters;
 
-use AndrewsChiozo\ApiCobrancaBb\Application\DTO\AutenticarDTO;
 /**
  * Serviço responsável por formatar os dados para a autenticação
  * no padrão exigido pela API do Banco do Brasil.
@@ -12,21 +11,27 @@ use AndrewsChiozo\ApiCobrancaBb\Application\DTO\AutenticarDTO;
 class AutenticarFormatter
 {
     /**
-     * Transforma os dados da Autenticação em um array compatível com o payload da API.
-     * @param AutenticarDTO $dto
-     * @return array Payload pronto para ser enviado via HTTP
+     * @param string $clientId
+     * @param string $clientSecret
+     * @return array
      */
-    public function format(AutenticarDTO $dto): array
+    public function header(string $clientId, string $clientSecret): array
     {
         return [
-            'headers' => [
-                'Authorization' => 'Basic ' . base64_encode($dto->clientId . ':' . $dto->clientSecret),
-                'Content-Type' => 'application/x-www-form-urlencoded'
-            ],
-            'form_params' => [
-                'grant_type' => 'client_credentials',
-                'scope' => $dto->scope
-            ]
+            'Authorization' => 'Basic ' . base64_encode($clientId . ':' . $clientSecret),
+            'Content-Type' => 'application/x-www-form-urlencoded'
+        ];
+    }
+
+    /**
+     * @param string $scope
+     * @return array
+     */
+    public function payload(string $scope): array
+    {
+        return [
+            'grant_type' => 'client_credentials',
+            'scope' => $scope
         ];
     }
 }
